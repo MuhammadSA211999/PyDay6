47370a41d5d17b17b377302b2b2688512acd6463
Muhammad SA <MuhammadSA211999>
2023-07-26 21:55:03 +0600

    search() method and its end(), span(), start() method

--- regex.py
+++ regex.py
@@ -7,18 +7,31 @@ search() => puru string er moddhye khuje, object return kore. sei object er modh
 
 search() er return kora object function gulu list-index return kore.
 
+search() string er moddhye pattern match holei true/false return korei string theke beriye ase. baki string er moddhye pattern check kore na.
+
 match() => sentence/string er protome khuje. boolean value true/false return kore.
 
 findall() => puru *string/sentence er modhye khuze. zekhane zekhane pattern match korbe sei pattern string niye ekti array/list return kore. 
 
 sub()=> sub() method *3 ti argument recieve kore. *sub(pattern, replaceValue, text). text sentence/string e *pattern match korle *replaceValue diye sei match kora ongsho ke replace kore dey.
 '''
-pattern='msa'
-text='My name is msa. MSA is an entrepreneur'
-if match(pattern,text):
-    print('// Matched')
-else:
-    print("// Don't matched")
+# pattern='msa'
+# text='My name is msa. MSA is an entrepreneur'
+# if match(pattern,text):
+#     print('// Matched')
+# else:
+#     print("// Don't matched")
 
 # don't matched // My name is msa. MSA is an entrepreneur
 # matched // MSA is an entrepreneur
+
+pattern='MSA'
+text='My name is MSA. MSA is an entrepreneur'
+matched=search(pattern,text)
+if matched:
+    whereStartMatch=matched.start() #11
+    whereEndMatch=matched.end() #14
+    startToEndMatched=matched.span() #(11,14)
+    print(whereStartMatch,whereEndMatch,startToEndMatched)
+else:
+    print("// Don't matched")
