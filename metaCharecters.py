3ffc7e6c6bf1dea44611f51821039e75c4ec6265
Muhammad SA <MuhammadSA211999>
2023-07-26 22:36:05 +0600

    regex sub() method

---
+++ metaCharecters.py
@@ -0,0 +1,2 @@
+# Meta Character with RegEx match() function
+from re import match
--- regex.py
+++ regex.py
@@ -14,36 +14,38 @@ match() => sentence/string er protome khuje. boolean value true/false return kor
 findall() => puru *string/sentence er modhye khuze. zekhane zekhane pattern match korbe sei pattern string niye ekti array/list return kore. 
 
 sub()=> sub() method *3 ti argument recieve kore. *sub(pattern, replaceValue, text). text sentence/string e *pattern match korle *replaceValue diye sei match kora ongsho ke replace kore dey.
+
+sub() method pattern diye replace howya notun string return kore.
+
 '''
 # MATCH()
-# pattern='msa'
-# text='My name is msa. MSA is an entrepreneur'
-# if match(pattern,text):
-#     print('// Matched')
-# else:
-#     print("// Don't matched")
-
+pattern='msa'
+text='My name is msa. MSA is an entrepreneur'
+if match(pattern,text):
+    print('// Matched')
+else:
+    print("// Don't matched")
 # don't matched // My name is msa. MSA is an entrepreneur
 # matched // MSA is an entrepreneur
 
 # SEARCH()
-# pattern='MSA'
-# text='My name is MSA. MSA is an entrepreneur'
-# matched=search(pattern,text)
-# if matched:
-#     whereStartMatch=matched.start() #11
-#     whereEndMatch=matched.end() #14
-#     startToEndMatched=matched.span() #(11,14)
-#     print(whereStartMatch,whereEndMatch,startToEndMatched)
-# else:
-#     print("// Don't matched")
+pattern='MSA'
+text='My name is MSA. MSA is an entrepreneur'
+matched=search(pattern,text)
+if matched:
+    whereStartMatch=matched.start() #11
+    whereEndMatch=matched.end() #14
+    startToEndMatched=matched.span() #(11,14)
+    print(whereStartMatch,whereEndMatch,startToEndMatched)
+else:
+    print("// Don't matched")
 
 # FINDALL()
-# pattern='MSA'
-# text='My name is MSA. MSA is an entrepreneur'
-# matched=findall(pattern,text)
-# if matched:
-#     print(matched)
+pattern='MSA'
+text='My name is MSA. MSA is an entrepreneur'
+matched=findall(pattern,text)
+if matched:
+    print(matched)
 # [MSA, MSA]
 
 
@@ -58,5 +60,22 @@ def isMatched(pattern,text):
 pattern='MSA'
 text='My name is MSA. MSA is an entrepreneur'
 howManyMatched=isMatched(pattern,text)
-print(howManyMatched)
+# print(howManyMatched)
 
+
+# functionally explained sub() method
+
+def replaceWithValue(pt,rpVl,txt):
+    if len(pt)>0 and len(rpVl)>0 and len(txt)>0:
+        replaced=sub(pt,rpVl,txt)
+        print(replaced)
+        return replaced
+    else:
+        return 'You have enter 3 value'
+
+pattern='msa'
+replaceValue='MSA'
+text='My name is msa. msa is an entrepreneur.'
+
+result=replaceWithValue(pattern,replaceValue,text)
+# print(result) My name is MSA. MSA is an entrepreneur.
